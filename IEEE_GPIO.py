ed86f82b86923b062ef5554642b56c3e5667be69
mahimachander <mchander6@gatech.edu>
2019-11-09 12:29:34 -0500

    updated libary paths

--- IEEE_GPIO.py
+++ IEEE_GPIO.py
@@ -1,5 +1,1 @@
-import sys
-sys.path.append('/opt/nvidia/jetson-gpio/lib/python/') #location of Jetson.GPIO library
-sys.path.append('/opt/nvidia/jetson-gpio/lib/python/Jetson/GPIO')
-import Jetson.GPIO as JGPIO
-import RPi.GPIO as RPiGPIO
+import Jetson.GPIO as GPIO
